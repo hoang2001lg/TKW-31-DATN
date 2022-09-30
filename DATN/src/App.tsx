f2688b2b6f9af1e78b0383a62aabf8c0b72f3618
thilv43 <thilv44443333@gmail.com>
2022-09-30 18:34:08 +0700

    git commit -m "ND-5 UI admin"

--- DATN/src/App.tsx
+++ DATN/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import AdminLayout from './layout/AdminLayout'
 import WebsiteLayout from './layout/WebsiteLayout'
 import HomePage from './page/HomePage'
 
@@ -11,6 +12,9 @@ function App() {
         <Routes>
           <Route path='/' element={< WebsiteLayout/>} >
               <Route index  element={<HomePage/>} />
+          </Route>
+          <Route path='/admin' element={ <AdminLayout />}> 
+
           </Route>
 
         </Routes>
