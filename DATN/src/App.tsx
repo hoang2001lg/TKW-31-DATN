f0549bdb8f804e819db8e6bc7ca9949f80c339e2
Tu Nguyen <nguyenminhtuk52@gmail.com>
2022-10-05 12:33:54 +0700

    ft:crud subject

--- DATN/src/App.tsx
+++ DATN/src/App.tsx
@@ -1,18 +1,54 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { addSubject, listSubject, removeSubject, updateSubject } from './api/subject'
 import './App.css'
+import AdminLayout from './layout/AdminLayout'
 import WebsiteLayout from './layout/WebsiteLayout'
+import Addsubject from './page/admin/subject/AddSubject'
+import Editsubject from './page/admin/subject/EditSubject'
+import ListSubject from './page/admin/subject/ListSubject'
 import HomePage from './page/HomePage'
+import { TypeSubject } from './Type/TypeSubject'
 
 function App() {
-
+//Subject start
+const [subjects, setSubjects] = useState<TypeSubject[]>([]);
+useEffect(() => {
+  const getSubject = async () => {
+    const { data } = await listSubject();
+    setSubjects(data);
+  };
+  getSubject();
+}, [])
+const onHandleRemoveSubject = (id: number) => {
+  if (window.confirm('Are you sure you want to remove  ?')) {
+    removeSubject(id);
+    setSubjects(subjects.filter(item => item.id !== id));
+  }
+}
+const onHandleAddSubject = async (subject: TypeSubject) => {
+  const { data } = await addSubject(subject);
+  setSubjects([...subjects, data]);
+  alert("Success!");
+}
+const onHandleUpdateSubject = async (subject: TypeSubject) => {
+  const { data } = await updateSubject(subject);
+  setSubjects(subjects.map(item => item.id == data.id ? data : item));
+}
+//Subject End
   return (
     <>
         <Routes>
           <Route path='/' element={< WebsiteLayout/>} >
               <Route index  element={<HomePage/>} />
           </Route>
-
+          <Route path="admin" element={< AdminLayout />}>
+          <Route path='subject'>
+            <Route index element={<ListSubject subjects={subjects} onRemovee={onHandleRemoveSubject} />} />
+            <Route path='add' element={<Addsubject onAddSubject={onHandleAddSubject} subjects={subjects} />} />
+            <Route path=':id/edit' element={<Editsubject onUpdateSubject={onHandleUpdateSubject} subjects={subjects} />} />
+          </Route>
+        </Route>
         </Routes>
     </>
   )
---
+++ DATN/src/page/admin/subject/ListSubject.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import { listSubject } from '../../../api/subject';
+import { TypeSubject } from '../../../Type/TypeSubject';
+
+
+type ListSubject = {
+  subjects: TypeSubject,
+  onRemovee: (category: TypeSubject) => void
+}
+function ListSubject(props: ListSubject) {
+  return (
+    <div>
+      <NavLink to='add'><button className=''>Add</button></NavLink>
+      <table >
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Tên</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+        {props.subjects?.map((item, index) => {
+            return <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{item.name}</td>
+              <td>
+              <Link to={`/admin/subject/${item.id}/edit`}><button >Sửa</button></Link>
+                <button style={{ marginLeft: '10px' }} onClick={() => props.onRemovee(item.id)} >Xóa</button>
+              </td>
+            </tr>
+          })}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+export default ListSubject
